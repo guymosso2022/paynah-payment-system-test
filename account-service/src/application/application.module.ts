63551fb64ed628902635abfb7d3dcb1fbea0c1b9
guymosso2022 <mossoguy2022@gmail.com>
2025-10-05 16:44:09 +0000

    feat: delete account credited -event file

--- account-service/src/application/application.module.ts
+++ account-service/src/application/application.module.ts
@@ -5,7 +5,6 @@ import { InfrastructureModule } from 'src/infrastructure/insfracture.module';
 import { CreditAccountHandler } from './commands/handlers/credit-account.handler';
 import { DebitAccountHandler } from './commands/handlers/debit-account.hanlder';
 import { GetAccountBalanceHandler } from './queries/handlers/get-account-balance.handler';
-import { AccountCreditedEventHandler } from './events/account-credited-event.handler';
 import { AccountEventPublisherService } from './services/account-event-publisher.service';
 
 export const CommandHandlers = [
@@ -15,21 +14,14 @@ export const CommandHandlers = [
 ];
 
 export const QueryHandlers = [GetAccountBalanceHandler];
-export const EventHandlers = [AccountCreditedEventHandler];
 export const Sagas = [];
 @Module({
   imports: [CqrsModule, InfrastructureModule],
   providers: [
     ...CommandHandlers,
     ...QueryHandlers,
-    ...EventHandlers,
     AccountEventPublisherService,
   ],
-  exports: [
-    ...CommandHandlers,
-    ...QueryHandlers,
-    ...EventHandlers,
-    AccountEventPublisherService,
-  ],
+  exports: [...CommandHandlers, ...QueryHandlers, AccountEventPublisherService],
 })
 export class ApplicationModule {}
