8c29adc2334cebc6a7c02b6a3707da7c7a9ea9ba
guymosso2022 <mossoguy2022@gmail.com>
2025-10-29 00:25:23 +0000

    fix(transaction-service): remove a commented function

--- transaction-service/src/infrastructure/repositories/prisma-transaction.repository.ts
+++ transaction-service/src/infrastructure/repositories/prisma-transaction.repository.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
 import { Transaction } from 'src/domain/entities/transaction.entity';
 import { ITransactionRepositoryPort } from 'src/domain/ports/transaction.port';
 import { AccountId } from 'src/domain/value-objects/account-id.vo';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PrismaTransactionRepository implements ITransactionRepositoryPort {
   constructor(private readonly prisma: PrismaService) {}
+
   async save(transaction: Transaction): Promise<Transaction> {
     await this.prisma.transaction.upsert({
       where: { id: transaction.getId().value },
@@ -40,74 +42,7 @@ export class PrismaTransactionRepository implements ITransactionRepositoryPort {
     return transaction;
   }
 
-  // async findById(
-  //   accountId: AccountId,
-  //   page = 1,
-  //   limit = 20,
-  // ): Promise<{
-  //   data: {
-  //     id: string;
-  //     amount: number;
-  //     currency: string;
-  //     type: string;
-  //     status: string;
-  //     accountId: string | null;
-  //     paymentId: string | null;
-  //     description: string | null;
-  //     createdAt: Date | null;
-  //     updatedAt: Date | null;
-  //   }[];
-  //   meta: {
-  //     firstPage: number;
-  //     total: number;
-  //     lastPage: number;
-  //     currentPage: number;
-  //     itemsPerPage: number;
-  //   };
-  // }> {
-  //   if (!accountId) {
-  //     throw new Error('AccountId is required');
-  //   }
-
-  //   const safePage = page && page > 0 ? page : 1;
-  //   const safeLimit = limit && limit > 0 ? limit : 20;
-  //   const skip = (safePage - 1) * safeLimit;
-
-  //   const [records, total] = await Promise.all([
-  //     this.prisma.transaction.findMany({
-  //       where: {
-  //         OR: [
-  //           { accountId: accountId.getValue() },
-  //           { targetAccountId: accountId.getValue() },
-  //         ],
-  //       },
-
-  //       skip,
-  //       take: safeLimit,
-  //       orderBy: { createdAt: 'desc' },
-  //     }),
-  //     this.prisma.transaction.count({
-  //       where: { accountId: accountId.getValue() },
-  //     }),
-  //   ]);
-
-  //   const meta = {
-  //     firstPage: 1,
-  //     total: total,
-  //     lastPage: Math.ceil(total / safeLimit),
-  //     currentPage: safePage,
-  //     itemsPerPage: safeLimit,
-  //   };
-
-  //   const transactions = records.map((record) => ({
-  //     ...record,
-  //     createdAt: record.createdAt ? new Date(record.createdAt) : null,
-  //     updatedAt: record.updatedAt ? new Date(record.updatedAt) : null,
-  //   }));
-
-  //   return { data: transactions, meta };
-  // }
-  async findById(
+  async findByAccountId(
     accountId: AccountId,
     page = 1,
     limit = 20,
